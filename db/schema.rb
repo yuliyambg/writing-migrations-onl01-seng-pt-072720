057b9d7ad7491fee87d021cc4820abeb96f770e9
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 02:38:26 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
-    t.integer "grade"
-    t.string "birthday"
   end
 
 end
