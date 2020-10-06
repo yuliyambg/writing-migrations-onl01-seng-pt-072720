09f19c85f13a92dc4b1ef780a4183b031d1f9935
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 02:26:02 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   def change
     change_column :students, :birthdate, :datetime
   end
